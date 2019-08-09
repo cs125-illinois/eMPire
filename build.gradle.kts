102960c767ec1e04c296a509f61321ee70e50988
Ben Nordick <Fleex255@users.noreply.github.com>
2019-08-09 17:20:43 -0500

    Allow use from a shadow JAR

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,21 @@
 plugins {
     id("java-library")
     kotlin("jvm") version "1.3.41"
+    id("com.github.johnrengelman.shadow") version "5.1.0"
 }
 
 dependencies {
     compileOnly(gradleApi())
-    implementation("com.android.tools.build:gradle:3.4.2")
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.3.41")
-    implementation("com.fasterxml.jackson.core:jackson-databind:2.9.9")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.9.8")
-    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.9.8")
-    implementation("org.ow2.asm:asm:7.1")
+    setOf(
+            "com.android.tools.build:gradle:3.4.2",
+            "org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.3.41",
+            "com.fasterxml.jackson.core:jackson-databind:2.9.9",
+            "com.fasterxml.jackson.module:jackson-module-kotlin:2.9.8",
+            "com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.9.8",
+            "org.ow2.asm:asm:7.1").forEach {
+        implementation(it)
+        shadow(it)
+    }
 }
 
 repositories {
