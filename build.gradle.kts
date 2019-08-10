41e443a8bba1cd535129d5bbb2c4d937e5a5f398
Ben Nordick <Fleex255@users.noreply.github.com>
2019-08-09 19:02:15 -0500

    Publish to Maven Central

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,13 @@
+import groovy.util.Node
+import java.net.URI
+import java.util.Properties
+
 plugins {
     id("java-library")
     kotlin("jvm") version "1.3.41"
     id("com.github.johnrengelman.shadow") version "5.1.0"
+    id("signing")
+    id("maven-publish")
 }
 
 dependencies {
@@ -28,5 +34,90 @@ tasks {
         kotlinOptions {
             jvmTarget = "1.8"
         }
+    }
+}
+
+// The rest of this file is configuration for publishing to Maven Central
+
+val sourceJar by tasks.registering(Jar::class) {
+    archiveClassifier.set("sources")
+    from(sourceSets["main"].allSource)
+}
+
+artifacts {
+    archives(tasks.getByName("jar"))
+    archives(sourceJar)
+}
+
+signing {
+    sign(configurations.getByName("archives")).forEach {
+        it.signatures.removeAll { oneSig ->
+            it.signatures.count { anotherSig -> oneSig.classifier == anotherSig.classifier } > 1
+        }
+    }
+}
+
+val properties = Properties().also { it.load(rootProject.file("local.properties").inputStream()) }
+
+publishing {
+    publications {
+        register("mavenJava", MavenPublication::class) {
+            pom.withXml {
+                asNode().apply {
+                    appendNode("description", "MP module manager")
+                    appendNode("name", "eMPire")
+                    appendNode("url", "https://github.com/cs125-illinois/eMPire")
+                    Node(this, "licenses").apply {
+                        Node(this, "license").apply {
+                            appendNode("name", "Apache License 2.0")
+                            appendNode("url", "https://www.apache.org/licenses/LICENSE-2.0.txt")
+                        }
+                    }
+                    Node(this, "scm").apply {
+                        appendNode("url", "https://github.com/cs125-illinois/eMPire")
+                        appendNode("connection", "scm:git:git://github.com/cs125-illinois/eMPire.git")
+                        appendNode("developerConnection", "scm:git:ssh://git@github.com:cs125-illinois/eMPire.git")
+                    }
+                    Node(this, "developers").apply {
+                        Node(this, "developer").apply {
+                            appendNode("name", "CS 125")
+                        }
+                    }
+                }
+            }
+            groupId = "com.github.cs125-illinois"
+            artifactId = "empire"
+            version = "1.0.0"
+            from(components.getByName("java"))
+            pom.withXml {
+                val pomFile = project.file("${project.buildDir}/generated-pom.xml")
+                pomFile.writeText(asString().toString().replace("<?xml version=\"1.0\"?>", "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"))
+                artifact(signing.sign(pomFile).signatureFiles.singleFile) {
+                    classifier = null
+                    extension = "pom.asc"
+                }
+            }
+            (tasks.getByName("signArchives") as Sign).signatureFiles.forEach {
+                artifact(it) {
+                    classifier = if (it.name.replace(".jar.asc", "").endsWith("sources")) "sources" else null
+                    extension = "jar.asc"
+                }
+            }
+        }
+    }
+    repositories {
+        maven {
+            url = URI("https://oss.sonatype.org/service/local/staging/deploy/maven2")
+            credentials {
+                username = properties["ossrh.username"] as? String
+                password = properties["ossrh.password"] as? String
+            }
+        }
+    }
+}
+
+afterEvaluate {
+    tasks.filter { it.name.startsWith("publishMavenJavaPublicationTo") }.forEach {
+        it.dependsOn("signArchives")
     }
 }
